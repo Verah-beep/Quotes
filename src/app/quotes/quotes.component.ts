ba6b9c03b5c622c2123960ed5ef8bab2b50c546f
verah-beep <veronikauma75@gmail.com>
2020-08-26 12:48:38 +0300

    add thefinal quote

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -12,7 +12,7 @@ export class QuotesComponent implements OnInit {
       new Quote(2, 'Talia', 'Tech Quote', 'Technology is always a two-edged sword.It will bring in many benefits, but also many disasters', 'Alan Moore', new Date(2019,09,11)),
       new Quote(3, 'Milcah', 'Funny Science and Technology', 'If you cant explain it simply,you dont understand it well enough', 'Albert Einstein', new Date(2020,3,12)),
       new Quote(4, 'Lovine', 'Success and Technology....', 'The technology you use impresses no one.The experience you create with it is everything.', 'Sean Gerety', new Date(2019,6,18)),
-      // new Quote(5, 'Kelvin', 'Motivational Quotes', 'Success is a lousy teacher.It seduces smart people into thinking they cant lose.', 'Bill Gates', new Date(2020, 02, 08), 0, 0),
+      new Quote(5, 'Kelvin', 'Motivational Quotes', 'Success is a lousy teacher.It seduces smart people into thinking they cant lose.', 'Bill Gates', new Date(2020,2,17)),
   ]
   constructor() { }
 
