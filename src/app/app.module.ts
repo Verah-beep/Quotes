729cec40792d74432c352644c1f1bc6c7097c7e0
verah-beep <veronikauma75@gmail.com>
2020-08-26 12:30:35 +0300

    Add files

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { QuotesDetailsComponent } from './quotes-details/quotes-details.component';
+import { QuotesComponent } from './quotes/quotes.component';
+import { QuotesFormComponent } from './quotes-form/quotes-form.component';
+import { CountInputComponent } from './count-input/count-input.component';
+import { Quote } from './quote.ts/quote.ts.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    QuotesDetailsComponent,
+    QuotesComponent,
+    QuotesFormComponent,
+    CountInputComponent,
+    Quote.TsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
