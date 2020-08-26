035b61487a534586c102401279c70c641fef73f6
verah-beep <veronikauma75@gmail.com>
2020-08-26 17:57:37 +0300

    add details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { QuotesDetailsComponent } from './quotes-details/quotes-details.component';
-import { QuotesComponent } from './quotes/quotes.component';
-import { QuotesFormComponent } from './quotes-form/quotes-form.component';
+import { QuoteComponent } from './quote/quote.component';
 import { CountInputComponent } from './count-input/count-input.component';
+import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { DateCountPipe } from './date-count.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuotesDetailsComponent,
-    QuotesComponent,
-    QuotesFormComponent,
+    QuoteComponent,
     CountInputComponent,
-  
+    QuoteDetailComponent,
+    QuoteFormComponent,
+    DateCountPipe
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     AppRoutingModule
   ],
