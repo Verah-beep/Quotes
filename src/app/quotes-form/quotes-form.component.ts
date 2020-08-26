36a19a5500004f69f2af9637222a8d02f08d6378
verah-beep <veronikauma75@gmail.com>
2020-08-26 16:46:15 +0300

    add a new date in a form

--- src/app/quotes-form/quotes-form.component.ts
+++ src/app/quotes-form/quotes-form.component.ts
@@ -1,11 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Quote } from "../quote"
 
 @Component({
-  selector: 'app-quotes-form',
-  templateUrl: './quotes-form.component.html',
-  styleUrls: ['./quotes-form.component.css']
+  selector: 'app-quote-form',
+  templateUrl: './quote-form.component.html',
+  styleUrls: ['./quote-form.component.css']
 })
-export class QuotesFormComponent implements OnInit {
+export class QuoteFormComponent implements OnInit {
+newQuote = new Quote( 0,"","",new Date());
+@Output() addQuote = new EventEmitter<Quote>();
+submitQuote(){
+  this.addQuote.emit(this.newQuote);
+}
+
 
   constructor() { }
 
